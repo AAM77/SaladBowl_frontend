a2493e7dc836b2712f93bd4288dd1035e766f82b
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-25 19:30:12 -0400

    Created page that displays an error for an unrecognized route.

---
+++ src/components/NoMatch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const SaladBars = () => {
+  return(
+    <div>
+      <h1>ERROR</h1>
+      <h1>THAT PAGE DOES NOT EXIST</h1>
+    </div>
+  )
+}
+
+export default SaladBars;
