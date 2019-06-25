2ec7ad54855e1fcf22f6395ea7408112ba6e9f88
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-25 01:02:17 -0400

    Added a fetch request to get the current user. Needed to add credentials: 'include' to all of the fetch requests for redux to retain the current user after adding it to componentDidMount.

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -15,11 +15,33 @@ export const setCurrentUser = user => {
 export const login = credentials => {
   return dispatch => {
     return fetch("http://localhost:3001/api/v1/login", {
+      credentials: "include",
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(credentials)
+    })
+      .then(response => response.json())
+      .then(user => {
+        if (user.error) {
+          alert(user.error)
+        } else {
+          dispatch(setCurrentUser(user))
+        }
+      })
+      .catch(console.log)
+  }
+}
+
+export const getCurrentUser = () => {
+  return dispatch => {
+    return fetch("http://localhost:3001/api/v1/get_current_user", {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
     })
       .then(response => response.json())
       .then(user => {
