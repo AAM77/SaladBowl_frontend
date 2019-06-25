8b35f14cf6797f381c351cacf11c32bb9f4e712d
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-25 19:40:59 -0400

    Added a signup page.

---
+++ src/components/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Signup = () => {
+  return(
+    <form>
+      <input type="text" placeholder="username" />
+      <input type="email" placeholder="email" />
+      <input type="password" placeholder="password" />
+    </form>
+  )
+}
+
+export default Signup;
