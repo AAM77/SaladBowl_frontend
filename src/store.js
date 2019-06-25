3c27a46f38445eafcafe59b2e3deb5b02b600c9a
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-24 23:11:41 -0400

    Importated the reducer into store.js.

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import usersReducer from './reducers/users.js';
-import currentUserReducer from './reducers/users.js';
+import currentUserReducer from './reducers/currentUser.js';
+import loginFormReducer from './reducers/loginForm.js'
 import thunk from 'redux-thunk';
 
 const reducer = combineReducers({
   users: usersReducer,
-  currentUser: currentUserReducer
+  currentUser: currentUserReducer,
+  loginForm: loginFormReducer
 })
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
