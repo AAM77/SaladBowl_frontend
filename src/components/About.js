bda988c07f6c75f67ae0be16053c32d984404ef0
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-26 17:17:44 -0400

    Added content to the about page and styled it using styled-components.

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  .about-container {
+    padding-top: 25px;
+    padding-bottom: 25px;
+    text-align: center;
+  }
+
+  .about-paragraph {
+    width: 545px;
+    display: inline-block;
+    padding-top: 25px;
+  }
+
+  h1, p {
+    color: #efefef
+  }
+`;
 
 
 const About = () => {
   return(
-    <div>
-      <h1>THE ABOUT PAGE</h1>
-    </div>
+    <Styles>
+      <div className="about-container">
+        <h1>About SaladBowl</h1>
+        <div className="about-paragraph">
+        <p>
+          We all want to eat healthy. Sometimes, though, it seems like too much work.
+          First, we have to figure out what to eat. Then, we need to figure out
+          whether we want to make something or purchase it. Once we figure that out,
+          we need to either learn how to make it or find a place that sells it.
+
+          Decisions upon decisions! It's so tiresome that we lose all motivation.
+
+          I created SaladBowl to help with this. It is meant to decrease the amount
+          of decisions you need to make so that you can get to having a healthy meal.
+        </p>
+        </div>
+      </div>
+    </Styles>
   )
 }
 
