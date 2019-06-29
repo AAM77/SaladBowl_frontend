4f32c53dd258e1a367aabefa3a630757ba4c2450
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-28 20:32:16 -0400

    Imported and added the  new user reducer to combineReducers.

--- src/store.js
+++ src/store.js
@@ -1,13 +1,15 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import usersReducer from './reducers/users.js';
 import currentUserReducer from './reducers/currentUser.js';
-import loginFormReducer from './reducers/loginForm.js'
+import loginFormReducer from './reducers/loginForm.js';
+import signupFormReducer from './reducers/signupForm.js';
 import thunk from 'redux-thunk';
 
 const reducer = combineReducers({
   users: usersReducer,
   currentUser: currentUserReducer,
-  loginForm: loginFormReducer
+  loginForm: loginFormReducer,
+  signupForm: signupFormReducer
 })
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
