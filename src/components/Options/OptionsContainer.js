3a67e1d9f26e7923b61cc03d4add2ef8d926c6df
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-30 02:34:19 -0400

    Moved styles into a separate file within the same directory. Differentiated the new file from the original file by using the original name and appending Styles to the end of it.

--- src/components/Options/OptionsContainer.js
+++ src/components/Options/OptionsContainer.js
@@ -1,38 +1,7 @@
 import React from 'react';
 import { ButtonToolbar, Button, Container, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-
-const Styles = styled.div`
-  #salad-bar-button {
-    display: inline-block;
-    padding: 25px 0px 50px 0px;
-  }
-
-  #user-page-div {
-    display: inline-block;
-    padding: 50px 0px 25px 0px;
-  }
-
-  #url-link {
-    text-decoration: none;
-  }
-
-  .rows-and-columns {
-    width: 1000px;
-  }
-
-  .row-of-columns {
-    padding: 50px 0px 0px 100px;
-  }
-
-
-  .column-text {
-    padding: 25px 0px 25px 0px;
-    border: 2px solid #FF4500;
-  }
-`;
-
+import { Styles } from './OptionsContainerStyles.js';
 
 const OptionsContainer = () => {
   return(
---
+++ src/components/Options/OptionsContainerStyles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Styles = styled.div`
+  #salad-bar-button {
+    display: inline-block;
+    padding: 25px 0px 50px 0px;
+  }
+
+  #user-page-div {
+    display: inline-block;
+    padding: 50px 0px 25px 0px;
+  }
+
+  #url-link {
+    text-decoration: none;
+  }
+
+  .rows-and-columns {
+    width: 1000px;
+  }
+
+  .row-of-columns {
+    padding: 50px 0px 0px 100px;
+  }
+
+
+  .column-text {
+    padding: 25px 0px 25px 0px;
+    border: 2px solid #FF4500;
+  }
+`;
