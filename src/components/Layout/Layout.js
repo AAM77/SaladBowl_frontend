c2297132b0059de45e4d5d67944f83bd4ff5697c
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-30 02:34:00 -0400

    Moved styles into a separate file within the same directory. Differentiated the new file from the original file by using the original name and appending Styles to the end of it.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import styled from 'styled-components';
-
-const Styles = styled.div`
-
-
-
-  .layout-background {
-    text-align: center;
-    height: 100%;
-    background: #222;
-  }
-`;
+import { Styles } from './LayoutStyles.js';
 
 
 
---
+++ src/components/Layout/LayoutStyles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Styles = styled.div`
+
+
+
+  .layout-background {
+    text-align: center;
+    height: 100%;
+    background: #222;
+  }
+`;
