01cc3d9620ee971f39ebcdb247a2d8b7995ba34e
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-30 01:43:53 -0400

    Renamed to LandingContainer since it will probably need to interact with Redux to determine if a user is logged in or not. Moved into a 'Landing' folder. Renaming it to Home might make its purpose clearer.

---
+++ src/components/Landing/LandingContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Login from '../Login/Login.js';
+import Logout from '../Login/Logout.js';
+import { connect } from 'react-redux';
+import { getCurrentUser } from '../../actions/currentUser.js';
+
+
+const LandingContainer = ( { currentUser } ) => {
+  return(
+    <div>
+      <h1>THE LANDING PAGE</h1>
+      {currentUser ? <Logout /> : <Login />}
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return{
+    currentUser: state.currentUser
+  }
+}
+
+export default connect(mapStateToProps, { getCurrentUser }) (LandingContainer);
