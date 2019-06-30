fbc87bde26e230a36fa300ef1dd0a85fe47f61a3
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-30 02:32:29 -0400

    Moved styles into a separate file within the same directory.

---
+++ src/components/Layout/BackgroundStyles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Styles = styled.div`
+
+  #background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: -4;
+    opacity: 0.7;
+
+    /* Preserve aspect ratio */
+    width: 100%;
+    height: 100%;
+  }
+`;
