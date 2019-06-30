2bf0ae0a92d5cc73f0bc3715bd7565345bef5879
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-30 02:33:48 -0400

    Moved styles into a separate file within the same directory. Differentiated the new file from the original file by using the original name and appending Styles to the end of it.

--- src/components/Layout/Jumbotron.js
+++ src/components/Layout/Jumbotron.js
@@ -1,43 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import styled from 'styled-components';
-import flowerSalad from '../../assets/images/flower_salad.jpg';
-import fruitSaladGalore from '../../assets/images/fruit_salad_galore.jpg';
-import greenSalad from '../../assets/images/green_salad.jpg';
-import makingSalad from '../../assets/images/making_salad.jpg';
-import saladBowls from '../../assets/images/salad_bowls.jpg';
-
-const images = [
-  flowerSalad,
-  fruitSaladGalore,
-  greenSalad,
-  makingSalad,
-  saladBowls
-]
-
-
-const Styles = styled.div`
-  .jumbo {
-    background: url(${images[Math.floor(Math.random() * images.length)]}) no-repeat fixed bottom;
-    background-size: cover;
-    color: #efefef;
-    height: 300px;
-    position: relative;
-    z-index: -2;
-  }
-
-  .overlay {
-    background-color: #000;
-    opacity: 0.6;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    z-index: -1;
-  }
-`;
+import { Styles } from './JumbotronStyles.js';
 
 
 export const BackgroundJumbotron = () => {
