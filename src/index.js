20fd88a86b2281423453c017acce82739c9e6423
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-30 01:53:42 -0400

    Changed the path names to reflect the new location of App.js.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import App from './App';
+import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import store from './store.js';
