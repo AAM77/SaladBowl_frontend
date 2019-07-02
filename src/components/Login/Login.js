0e22d55782effcb05daebc5f7bdf84f9f6a49758
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-01 22:23:21 -0400

    Moved all of the logic, state, and dispatch code to the LoginContainer so that the 'Login' component is now responsible only for displaying the form.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,42 +1,44 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { updateLoginForm } from '../../actions/loginForm.js';
-import { login } from "../../actions/currentUser.js";
+// import { connect } from 'react-redux';
+// import { updateLoginForm } from '../../actions/loginForm.js';
+// import { login } from "../../actions/currentUser.js";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import { Styles } from './LoginStyles.js';
+//import LoginContainer from '../Login/LoginContainer.js';
 
-const Login = ( { loginFormData, updateLoginForm, login } ) => {
+const Login = ( { loginFormData, updateLoginForm, login, handleSubmit, handleInputChange } ) => {
 
-  const handleInputChange = event => {
-    const { name, value } = event.target
-
-    const updatedFormInfo = {
-      ...loginFormData,
-      [name]: value
-    }
-    updateLoginForm(updatedFormInfo)
-  }
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    login(loginFormData)
-  }
+  // const handleInputChange = event => {
+  //   const { name, value } = event.target
+  //
+  //   const updatedFormInfo = {
+  //     ...loginFormData,
+  //     [name]: value
+  //   }
+  //   updateLoginForm(updatedFormInfo)
+  // }
+  //
+  // const handleSubmit = event => {
+  //   event.preventDefault();
+  //   login(loginFormData)
+  //   return(<LoginContainer />)
+  // }
 
   return(
     <Styles>
       <div className="login-form">
-        <Form  onSubmit={handleSubmit}>
+        <Form  onSubmit={(event) => handleSubmit(event)}>
 
           <Form.Group controlId="formBasicUsername">
             <Form.Label>Username</Form.Label>
-            <Form.Control placeholder="Enter Username" value={loginFormData.username} name="username" type="text" onChange={handleInputChange} />
+            <Form.Control placeholder="Enter Username" value={loginFormData.username} name="username" type="text" onChange={(event) => handleInputChange(event)} />
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control placeholder="Enter Username" value={loginFormData.password} name="password" type="password" onChange={handleInputChange} />
+            <Form.Control placeholder="Enter Username" value={loginFormData.password} name="password" type="password" onChange={(event) => handleInputChange(event)} />
           </Form.Group>
 
           <Button variant="primary" value="Log In" type="submit">
@@ -54,10 +56,4 @@ const Login = ( { loginFormData, updateLoginForm, login } ) => {
   )
 }
 
-const mapStateToProps = state => {
-  return {
-    loginFormData: state.loginForm
-  }
-}
-
-export default connect(mapStateToProps, { updateLoginForm, login }) (Login);
+export default Login;
--- src/components/Login/LoginContainer.js
+++ src/components/Login/LoginContainer.js
@@ -1,23 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Login from './Login.js';
 import Logout from './Logout.js';
 import { connect } from 'react-redux';
-import { getCurrentUser } from '../../actions/currentUser.js';
+import { login, getCurrentUser } from '../../actions/currentUser.js';
+import { updateLoginForm } from '../../actions/loginForm.js';
 
 
-const LandingPage = ( { currentUser } ) => {
-  return(
-    <div>
-      <h1>Sign In</h1>
-      {currentUser ? <Logout /> : <Login />}
-    </div>
-  )
+class LandingPage extends Component {
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target
+
+    const updatedFormInfo = {
+      ...this.props.loginFormData,
+      [name]: value
+    }
+    this.props.updateLoginForm(updatedFormInfo)
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.login(this.props.loginFormData, this.props.history)
+    //return(<LoginContainer />)
+  }
+  // = ( { currentUser, loginFormData, updateLoginForm, login } ) =>
+
+  render() {
+    const {
+      currentUser,
+      loginFormData,
+      updateLoginForm,
+      login
+    } = this.props
+
+    return(
+      <div>
+        <h1>Sign In</h1>
+        {currentUser ? <Logout /> :
+          <Login
+            loginFormData={loginFormData}
+            updateLoginForm={updateLoginForm}
+            login={login}
+            handleSubmit={this.handleSubmit}
+            handleInputChange={this.handleInputChange}
+           />
+         }
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
   return{
+    loginFormData: state.loginForm,
     currentUser: state.currentUser
   }
 }
 
-export default connect(mapStateToProps, { getCurrentUser }) (LandingPage);
+export default connect(mapStateToProps, { login, updateLoginForm, getCurrentUser }) (LandingPage);
