18a63cd9de9d0dd96498d5b85c2e7c8809531a38
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-01 22:21:31 -0400

    imported resetForm and setCurrentUser. Modified the 'signup' asynchronous action creator to require 'history' as a second argument. Also changed the fetch route. Added credentials: include. Renamed 'user' in the second .then() to 'response' & it now alerts when there is an error. Dispatched more actions upon return of a successful response. It logs the user in upon successful registration. Added a history.push to redirect to the user's home page after success.

--- src/actions/newUser.js
+++ src/actions/newUser.js
@@ -1,3 +1,6 @@
+import { resetForm } from './resetForm.js';
+import { setCurrentUser } from './currentUser.js';
+
 /////////////////////////////////
 // Synchronous Action Creators //
 /////////////////////////////////
@@ -12,22 +15,26 @@ export const createNewUser = user => {
 // Asynchronous Action Creators //
 //////////////////////////////////
 
-export const signup = (credentials) => {
-  console.log(credentials)
+export const signup = (credentials, history) => {
   return dispatch => {
-    return fetch("http://localhost:3001/api/v1/users", {
+    return fetch("http://localhost:3001/api/v1/register", {
+      credentials: "include",
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(credentials)
     })
-      .then(response => response.json())
-      .then(user => {
-        if (user.error) {
-          alert(user.error)
+      .then(r => r.json())
+      .then( response => {
+        if (response.error) {
+          alert(response.error)
         } else {
-          dispatch(createNewUser(user))
+          dispatch(createNewUser(response))
+          debugger;
+          dispatch(setCurrentUser(response))
+          dispatch(resetForm())
+          history.push(`/users/${response.id}`)
         }
       })
       .catch(console.log)
