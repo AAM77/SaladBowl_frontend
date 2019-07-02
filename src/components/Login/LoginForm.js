433133953b6e31e93fd3d1864630c9dd533c8d07
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-01 23:05:39 -0400

    Removed (unnecessary) commented out code.

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,30 +1,10 @@
 import React from 'react';
-// import { connect } from 'react-redux';
-// import { updateLoginForm } from '../../actions/loginForm.js';
-// import { login } from "../../actions/currentUser.js";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import { Styles } from './LoginStyles.js';
-//import LoginContainer from '../Login/LoginContainer.js';
 
 const LoginForm = ( { loginFormData, updateLoginForm, login, handleSubmit, handleInputChange } ) => {
-
-  // const handleInputChange = event => {
-  //   const { name, value } = event.target
-  //
-  //   const updatedFormInfo = {
-  //     ...loginFormData,
-  //     [name]: value
-  //   }
-  //   updateLoginForm(updatedFormInfo)
-  // }
-  //
-  // const handleSubmit = event => {
-  //   event.preventDefault();
-  //   login(loginFormData)
-  //   return(<LoginContainer />)
-  // }
 
   return(
     <Styles>
