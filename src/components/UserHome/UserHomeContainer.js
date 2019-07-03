91afd22043c3b0e7b72c4fd18e5c4ab96ed36029
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-03 05:58:41 -0400

    imported withRouter, changed the component into a container/smart component, imported connect, mapped state to props, and used a ternary to conditionally render the page. It forcefully navigates a user away from the page if not the current user.

--- src/components/UserHome/UserHomeContainer.js
+++ src/components/UserHome/UserHomeContainer.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ButtonToolbar, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { Styles } from './UserHomeContainerStyles.js';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 
-const UserHomeContainer = () => {
-  return(
-    <Styles>
-      <div id="user-page-div">
-        <h1>Welcome, User!</h1>
-        <h4>Your location: U.S.</h4>
-        <div id="salad-bar-button">
-          <Link to="/register" id="url-link">
-            <ButtonToolbar>
-              <Button variant="warning">Find a Salad Bar Near You</Button>
-            </ButtonToolbar>
-          </Link>
-        </div>
-      </div>
+class UserHomeContainer extends Component {
+  render() {
+    return(
+      <>
+        {
+          this.props.currentUser ?
 
-    </Styles>
-  )
+          <Styles>
+            <div id="user-page-div">
+              <h1>Welcome, {this.props.currentUser.username}!</h1>
+              <h4>Your location: U.S.</h4>
+              <div id="salad-bar-button">
+                <Link to="/saladbars" id="url-link">
+                  <ButtonToolbar>
+                    <Button variant="warning">Find a Salad Bar Near You</Button>
+                  </ButtonToolbar>
+                </Link>
+              </div>
+            </div>
+          </Styles> :
+
+          this.props.history.push('/')
+        }
+      </>
+
+    )
+  }
 }
 
-export default withRouter(UserHomeContainer);
+const mapStateToProps = (state) => {
+  return { currentUser: state.currentUser }
+}
+
+export default withRouter(connect(mapStateToProps)(UserHomeContainer));
