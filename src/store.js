f1c7a09ce70bd0e1c21f24d3e5b43011e723f531
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-03 23:47:50 -0400

    Updated the store to combine the meal type and salad bowls reducers.

--- src/store.js
+++ src/store.js
@@ -3,13 +3,17 @@ import usersReducer from './reducers/users.js';
 import currentUserReducer from './reducers/currentUser.js';
 import loginFormReducer from './reducers/loginForm.js';
 import signupFormReducer from './reducers/signupForm.js';
+import saladBowlsReducer from './reducers/salad_bowls.js';
+import mealTypeReducer from './reducers/mealType.js';
 import thunk from 'redux-thunk';
 
 const reducer = combineReducers({
   users: usersReducer,
   currentUser: currentUserReducer,
   loginForm: loginFormReducer,
-  signupForm: signupFormReducer
+  signupForm: signupFormReducer,
+  mealType: mealTypeReducer,
+  saladBowls: saladBowlsReducer
 })
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
