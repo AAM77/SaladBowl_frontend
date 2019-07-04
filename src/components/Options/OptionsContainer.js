03f4f6a38ec68b16a965225c1df420dece40aba6
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-03 23:56:04 -0400

    Began splitting the options container into multiple components. Also mapped the meal type and filtered salad bowls from the store state to props.

--- src/components/Options/OptionsContainer.js
+++ src/components/Options/OptionsContainer.js
@@ -1,34 +1,74 @@
-import React from 'react';
-import { ButtonToolbar, Button, Container, Col, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+//import { Container, Col, Row } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
 import { Styles } from './OptionsContainerStyles.js';
+import { connect } from 'react-redux';
+import { getAllSaladBowls } from '../../actions/salad_bowls.js';
+import BreakfastSaladBowls from './BreakfastSaladBowls.js';
+import LunchSaladBowls from './BreakfastSaladBowls.js';
+import DinnerSaladBowls from './BreakfastSaladBowls.js';
 
-const OptionsContainer = () => {
-  return(
-    <Styles>
-      <div className="rows-and-columns">
-        <Container>
-          <Row className="row-of-columns">
-            <Col sm className="column-text">HOW ARE YOU 1 of 4 HOW ARE YOU</Col>
-            <Col sm className="column-text">HOW ARE YOU 2 of 4 HOW ARE YOU</Col>
-            <Col sm className="column-text">HOW ARE YOU 3 of 4 HOW ARE YOU</Col>
-            <Col sm className="column-text">HOW ARE YOU 4 of 4 HOW ARE YOU</Col>
-          </Row>
-        </Container>
-      </div>
-      <div id="user-page-div">
-        <h1>Welcome, User!</h1>
-        <h4>Your location: U.S.</h4>
-        <div id="salad-bar-button">
-          <Link to="/register" id="url-link">
-            <ButtonToolbar>
-              <Button variant="warning">Find a Salad Bar Near You</Button>
-            </ButtonToolbar>
-          </Link>
+class OptionsContainer extends Component {
+
+  componentDidMount() {
+    this.props.getAllSaladBowls();
+  }
+
+  mealSelection = () => {
+    let renderedComponent;
+
+    if (this.props.mealType === "breakfast") {
+      renderedComponent = <BreakfastSaladBowls allBreakfastBowls={this.props.allBreakfastBowls} />
+    } else if (this.props.mealType === "lunch") {
+      renderedComponent =  <LunchSaladBowls allLunchBowls={this.props.allLunchBowls} />
+    } else if (this.props.mealType === "dinner") {
+      renderedComponent =  <DinnerSaladBowls allDinnerBowls={this.props.allDinnerBowls}  />
+    } else {
+      renderedComponent = <h1>No Meals</h1>
+    }
+
+    return renderedComponent
+  }
+
+  render() {
+    return(
+      <Styles>
+        {this.mealSelection()}
+
+        <div>
+          {
+            this.props.allSaladBowls ?
+            <ul>
+
+            </ul> :
+            <p>Loading All Salad Bowls</p>
+          }
         </div>
-      </div>
-    </Styles>
-  )
+      </Styles>
+    )
+  }
+}
+//
+// {
+//   this.props.allBreakfastBowls ?
+//   <div className="rows-and-columns">
+//     <Container>
+//       <Row className="row-of-columns">
+//         {this.breakfastBowls()}
+//       </Row>
+//     </Container>
+//   </div> :
+//   <p>Loading items</p>
+// }
+
+const mapStateToProps = (state) => {
+  return {
+    mealType: state.mealType,
+    allSaladBowls: state.saladBowls,
+    allBreakfastBowls: state.saladBowls.filter( saladBowl => saladBowl.meal_time_frame === 'breakfast'),
+    allLunchBowls: state.saladBowls.filter( saladBowl => saladBowl.meal_time_frame === 'lunch'),
+    allDinnerBowls: state.saladBowls.filter( saladBowl => saladBowl.meal_time_frame === 'dinner')
+  }
 }
 
-export default OptionsContainer;
+export default connect(mapStateToProps, { getAllSaladBowls }) (OptionsContainer);
