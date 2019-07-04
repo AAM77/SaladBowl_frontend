3c91b3f1f292f834ace8136d19c4bfeca863ee9b
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-04 15:28:55 -0400

    Consolidated the Breakfast, Lunch, & Dinner SaladBowls components into one to reduce code smell.

--- src/components/Options/SaladBowls.js
+++ src/components/Options/SaladBowls.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import SaladBowlCard from './SaladBowlCard.js'
 
+const LunchSaladBowls = ( { saladBowls } ) => {
 
-const SaladBowls = () => {
+  const parseSaladBowls = () => {
+    return saladBowls.map ( saladBowl => <Col sm className="column-text" key={saladBowl.id}><SaladBowlCard saladBowl={saladBowl} /></Col>)
+  }
   return(
     <div>
-      <h1>TODAY'S SELECTION OF SALAD BOWLS</h1>
+      {
+        saladBowls ?
+        <div className="rows-and-columns">
+          <Container>
+            <Row className="row-of-columns">
+              {parseSaladBowls()}
+            </Row>
+          </Container>
+        </div> :
+        <p>Loading items</p>
+      }
     </div>
   )
 }
 
-export default SaladBowls;
+export default LunchSaladBowls;
