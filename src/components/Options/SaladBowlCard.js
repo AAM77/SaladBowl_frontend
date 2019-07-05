c06e0f0649e985bfdb9b66224733b1b2a3bfa7f6
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-05 18:10:46 -0400

    Added an ingredients list for the salad bowl.

--- src/components/Options/SaladBowlCard.js
+++ src/components/Options/SaladBowlCard.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 
 const SaladBowlCard = ( { saladBowl }) => {
+
+  const ingredients = () => saladBowl.ingredients.map( ingredient => <li>{ingredient.name}</li>)
+
   return(
     <div>
       <h5>{saladBowl.name} {saladBowl.id}</h5>
+      <div>
+        <ul>
+          {ingredients()}
+        </ul>
+      </div>
     </div>
   )
 }
