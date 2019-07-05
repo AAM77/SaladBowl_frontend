96911f59e0012519488130759124b667159d9d20
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-05 18:14:59 -0400

    imported components from the google-maps-react library/package. Added a basic google map to test that it works. Added styling. Wrapped the component name inside of GoogleApiWrapper for export default.

--- src/components/Options/SaladBars.js
+++ src/components/Options/SaladBars.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 
-const SaladBars = () => {
+
+const SaladBars = (props) => {
+
+  const mapStyles = {
+    width: '500px',
+    height: '500px',
+  };
+
   return(
     <div>
       <h1>SALADBARS NEAR YOU</h1>
+
+      <Map
+        google={props.google}
+        zoom={8}
+        style={mapStyles}
+        initialCenter={{ lat: 47.444, lng: -122.176}}
+      />
+
+      <Marker position={{ lat: 48.00, lng: -122.00}} />
     </div>
   )
 }
 
-export default SaladBars;
+const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+
+export default GoogleApiWrapper({ apiKey: API_KEY })(SaladBars);
