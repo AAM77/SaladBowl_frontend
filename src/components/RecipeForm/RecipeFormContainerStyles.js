de707f980d9dbd543b96898beb13db956a6f31f9
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-06 22:39:11 -0400

    Added styles for the RecipeFormContainer.

---
+++ src/components/RecipeForm/RecipeFormContainerStyles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Styles = styled.div`
+  .recipe-form {
+    display: inline-block;
+    padding-bottom: 56px;
+  }
+
+  h3 {
+    color: #efefef
+  }
+`;
