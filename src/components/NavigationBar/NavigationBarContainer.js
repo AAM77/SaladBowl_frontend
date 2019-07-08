8aa4ea8292e70f845eecaf822fc0965c4803acae
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-08 12:34:12 -0400

    Modified a route in the navigation bar container to route to /actions.

--- src/components/NavigationBar/NavigationBarContainer.js
+++ src/components/NavigationBar/NavigationBarContainer.js
@@ -37,7 +37,7 @@ class NavigationBarContainer extends Component {
           <NavDropdown.Divider />
           <NavDropdown.Divider />
           <Dropdown.Item as={Link} to="/">Favorite Bowls</Dropdown.Item>
-          <Dropdown.Item as={Link} to="/">Account</Dropdown.Item>
+          <Dropdown.Item as={Link} to="/account">Account</Dropdown.Item>
           <Dropdown.Item onClick={this.handleLogoutClick}>Log Out</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
