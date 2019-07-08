48faa5912d763849ece3e9cfc582059bee9571fb
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-08 13:16:27 -0400

    Added styles for the Account Containter.

---
+++ src/components/Account/AccountContainerStyles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Styles = styled.div`
+  .account-form {
+    display: inline-block;
+    padding-bottom: 56px;
+  }
+
+  h3 {
+    color: #efefef
+  }
+
+  Form {
+    color: #efefef;
+  }
+`;
