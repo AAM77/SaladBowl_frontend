c6d3c6cddf012bff4d9640a5dfe4ead156c446b8
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-08 13:16:10 -0400

    Imported appropriate components (action, reducer, etx.) and added a form to update user info.

--- src/components/Account/AccountContainer.js
+++ src/components/Account/AccountContainer.js
@@ -1,12 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { getCurrentUser } from '../../actions/currentUser.js';
+import { updateAccountForm } from '../../actions/accountForm.js';
+import { updateAccountDetails } from '../../actions/updateAccount.js';
+import { connect } from 'react-redux';
 
+import { Styles } from './AccountContainerStyles.js';
 
-const AccountContainer = () => {
-  return(
-    <div>
-     <h1>THE ACCOUNT DETAILS PAGE</h1>
-    </div>
-  )
+class AccountContainer extends Component {
+
+  handleOnChange = (event) => {
+
+    const { name, value } = event.target;
+    const updatedAccountInfo = {
+      ...this.props.accountDetails,
+      [name]: value
+    }
+
+    this.props.updateAccountForm(updatedAccountInfo);
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    this.props.updateAccountDetails(this.props.accountDetails, this.props.currentUser.id, this.props.history);
+  }
+
+  render() {
+    return(
+      <Styles>
+        { this.props.currentUser ?
+
+          <div className="account-form">
+            <h1>{this.props.currentUser.username} Account Details</h1>
+            <br />
+            <Form onSubmit={(event) => this.handleOnSubmit(event)}>
+              <h4>Authentication</h4>
+
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control placeholder="Enter an email" defaultValue={this.props.currentUser.email} name="email" type="email" onChange={(event) => this.handleOnChange(event)} required />
+              </Form.Group>
+
+              <h4>Personal Information</h4>
+              <Form.Group controlId="formBasicFirstName">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control placeholder="First Name" defaultValue={this.props.currentUser.first_name} name="first_name" type="text" onChange={(event) => this.handleOnChange(event)} />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicLastName">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control placeholder="Last Name" defaultValue={this.props.currentUser.last_name} name="last_name" type="text" onChange={(event) => this.handleOnChange(event)} />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicAddress">
+                <Form.Label>Address</Form.Label>
+                <Form.Control placeholder="Address" defaultValue={this.props.currentUser.address} name="address" type="text" onChange={(event) => this.handleOnChange(event)} />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicZipcode">
+                <Form.Label>Zipcode</Form.Label>
+                <Form.Control placeholder="Zipcode" defaultValue={this.props.currentUser.zipcode} name="zipcode" type="text" onChange={(event) => this.handleOnChange(event)} />
+              </Form.Group>
+
+              <Button variant="primary" defaultValue="Update Details" type="submit">
+                Update
+              </Button>
+            </Form>
+          </div>
+          :
+          <p>No One is Logged In</p>
+        }
+      </Styles>
+    )
+  }
 }
 
-export default AccountContainer;
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.currentUser,
+    accountDetails: state.accountForm
+   }
+}
+
+export default connect(mapStateToProps, { getCurrentUser, updateAccountDetails, updateAccountForm })(AccountContainer);
