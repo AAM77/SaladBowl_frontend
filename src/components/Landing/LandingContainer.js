e9d4e43a48270c32ee8f48ea26bc50ecad9058ae
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-09 23:33:52 -0400

    Modified what gets displayed on the landing page.

--- src/components/Landing/LandingContainer.js
+++ src/components/Landing/LandingContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import simpleSalad from '../../assets/images/simple_salad_bowl.jpg';
+import { Styles } from './LandingContainerStyles.js';
 
 
 class LandingContainer extends Component {
@@ -12,10 +14,17 @@ class LandingContainer extends Component {
     } = this.props
 
     return(
-      <div>
-        <h1>THE LANDING PAGE</h1>
-        {currentUser ? history.push(`/users/${currentUser.id}`) : <h3>Please Sign in</h3>}
-      </div>
+      <Styles>
+        <div>
+          <h1>Welcome to SaladBowl!</h1>
+          <br />
+          <img src={simpleSalad} alt='a simple salad bowl' />
+          <br />
+          <h3>Let us help you decide what to have for breakfast, lunch, and dinner!</h3>
+          <br />
+          {currentUser ? history.push(`/users/${currentUser.id}`) : <h3>Please Sign in</h3>}
+        </div>
+      </Styles>
     )
   }
 }
