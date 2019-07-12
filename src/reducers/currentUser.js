de50f7ee0bbe422896d20024955ac5654ca69cc2
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-11 23:23:25 -0400

    refactored to add an initialState with a currentUser property. Then, changed the return values so that the current state gets copied before being modified.

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,9 +1,13 @@
-export default (state = {}, action) => {
+const initialState = {
+  currentUser: ""
+}
+
+export default (state = initialState, action) => {
   switch (action.type) {
     case "SET_CURRENT_USER":
-      return action.user
+      return {...state, currentUser: action.user}
     case "LOGOUT_CURRENT_USER":
-      return ""
+      return {currentUser: ""}
     default:
       return state
   }
