53aad3b92c8ca6953fae398b5ebdf4cdd09818b3
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-11 23:14:20 -0400

    created an initialState for the state & returned a proper object that copies the initial state.

--- src/reducers/salad_bowls.js
+++ src/reducers/salad_bowls.js
@@ -1,7 +1,18 @@
-export default (state = [], action) => {
+const initialState = {
+  breakfastBowls: [],
+  lunchBowls: [],
+  dinnerBowls: []
+}
+
+export default (state = initialState, action) => {
   switch (action.type) {
     case 'GET_ALL_SALAD_BOWLS':
-      return action.saladBowls
+      return {
+        ...state,
+        breakfastBowls: action.saladBowls.filter(saladBowl => saladBowl.meal_time_frame === 'breakfast'),
+        lunchBowls: action.saladBowls.filter(saladBowl => saladBowl.meal_time_frame === 'lunch'),
+        dinnerBowls: action.saladBowls.filter(saladBowl => saladBowl.meal_time_frame === 'dinner')
+      }
     default:
       return state
   }
