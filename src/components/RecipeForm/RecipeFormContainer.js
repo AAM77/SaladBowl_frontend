9c30300edf1448f1c1a3edabd1fc46a79d34a5a2
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-14 17:44:33 -0400

    Changed the name of the prop representing the mapped state.

--- src/components/RecipeForm/RecipeFormContainer.js
+++ src/components/RecipeForm/RecipeFormContainer.js
@@ -7,23 +7,21 @@ import { updateRecipeForm } from '../../actions/recipeForm.js';
 import { createNewRecipe } from '../../actions/recipe.js';
 
 
-
 class RecipeFormContainer extends Component {
 
   handleOnChange = (event) => {
     const { name, value } = event.target;
 
     const updatedRecipeForm = {
-      ...this.props.recipeFormData,
+      ...this.props.recipeData,
       [name]: value
     }
-
     this.props.updateRecipeForm(updatedRecipeForm)
   }
 
   handleOnSubmit = (event) => {
     event.preventDefault();
-    this.props.createNewRecipe(this.props.recipeFormData, this.props.history)
+    this.props.createNewRecipe(this.props.recipeData, this.props.history)
   }
 
   render() {
@@ -35,65 +33,65 @@ class RecipeFormContainer extends Component {
 
             <Form.Group controlId="formBasicRecipeName">
               <Form.Label>Recipe Name</Form.Label>
-              <Form.Control placeholder="Enter a recipe name" defaultValue="" name="name" type="text" onChange={(event) => this.handleOnChange(event)} required/>
+              <Form.Control placeholder="Enter a recipe name" value={this.props.recipeData.name} name="name" type="text" onChange={(event) => this.handleOnChange(event)} required/>
             </Form.Group>
 
             <Form.Group controlId="formBasicUrl">
               <Form.Label>URL</Form.Label>
-              <Form.Control placeholder="Recipe URL" defaultValue="" name="url" type="text" onChange={(event) => this.handleOnChange(event)} />
+              <Form.Control placeholder="Recipe URL" value={this.props.recipeData.url} name="url" type="text" onChange={(event) => this.handleOnChange(event)} />
             </Form.Group>
 
             <Form.Group controlId="exampleForm.ControlSelect1">
               <Form.Label>Meal Type</Form.Label>
               <Form.Control as="select" placeholder="Select Meal Type" name="meal_time_frame" onChange={(event) => this.handleOnChange(event)} required>
-                <option defaultValue="">Select One</option>
-                <option defaultValue="breakfast">breakfast</option>
-                <option defaultValue="lunch">lunch</option>
-                <option defaultValue="dinner">dinner</option>
+                <option value="">Select One</option>
+                <option value="breakfast">breakfast</option>
+                <option value="lunch">lunch</option>
+                <option value="dinner">dinner</option>
               </Form.Control>
             </Form.Group>
 
             <Form.Group controlId="formBasicCalories">
               <Form.Label>Calories</Form.Label>
-              <Form.Control placeholder="Calories per serving" defaultValue="" name="calories" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
+              <Form.Control placeholder="Calories per serving" value={this.props.recipeData.calories} name="calories" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
             </Form.Group>
 
             <Form.Group controlId="formBasicFat">
               <Form.Label>Total Fat</Form.Label>
-              <Form.Control placeholder="Total Fat per serving" defaultValue="" name="fat" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
+              <Form.Control placeholder="Total Fat per serving" value={this.props.recipeData.fat} name="fat" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
             </Form.Group>
 
             <Form.Group controlId="formBasicCholesterol">
               <Form.Label>Cholesterol</Form.Label>
-              <Form.Control placeholder="Cholesterol per serving" defaultValue="" name="cholesterol" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
+              <Form.Control placeholder="Cholesterol per serving" value={this.props.recipeData.cholesterol} name="cholesterol" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
             </Form.Group>
 
             <Form.Group controlId="formBasicSodium">
               <Form.Label>Sodium</Form.Label>
-              <Form.Control placeholder="Sodium per serving" defaultValue="" name="sodium" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
+              <Form.Control placeholder="Sodium per serving" value={this.props.recipeData.sodium} name="sodium" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
             </Form.Group>
 
             <Form.Group controlId="formBasicCarbohydrates">
               <Form.Label>Carbohydrates</Form.Label>
-              <Form.Control placeholder="Carbohydrates" defaultValue="" name="carbohydrates" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
+              <Form.Control placeholder="Carbohydrates" value={this.props.recipeData.carbohydrates} name="carbohydrates" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
             </Form.Group>
 
             <Form.Group controlId="formBasicSugar">
               <Form.Label>Sugars</Form.Label>
-              <Form.Control placeholder="Sugars per serving" defaultValue="" name="sugar" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
+              <Form.Control placeholder="Sugars per serving" value={this.props.recipeData.sugar} name="sugar" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
             </Form.Group>
 
             <Form.Group controlId="formBasicFiber">
               <Form.Label>Fiber</Form.Label>
-              <Form.Control placeholder="Fiber per serving" defaultValue="" name="fiber" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
+              <Form.Control placeholder="Fiber per serving" value={this.props.recipeData.fiber} name="fiber" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
             </Form.Group>
 
             <Form.Group controlId="formBasicProtein">
               <Form.Label>Protein</Form.Label>
-              <Form.Control placeholder="Protein per serving" defaultValue="" name="protein" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
+              <Form.Control placeholder="Protein per serving" value={this.props.recipeData.protein} name="protein" type="number" step="0.01" onChange={(event) => this.handleOnChange(event)} />
             </Form.Group>
 
-            <Button variant="primary" defaultValue="Submit Recipe" type="submit">
+            <Button variant="primary" value="Submit Recipe" type="submit">
               Submit Recipe
             </Button>
           </Form>
@@ -107,7 +105,7 @@ class RecipeFormContainer extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return { recipeFormData: state.recipeForm }
+  return { recipeData: state.recipeForm.recipeDetails }
 }
 
 export default connect(mapStateToProps, { updateRecipeForm, createNewRecipe })(RecipeFormContainer)
